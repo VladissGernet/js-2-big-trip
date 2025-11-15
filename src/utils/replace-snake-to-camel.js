826e15a76ea672081b7512196c03381f85d20938
VladissGernet <skeimorr@gmail.com>
2025-11-15 15:53:46 +0400

    Упростил конвертацию данных

--- src/utils/replace-snake-to-camel.js
+++ src/utils/replace-snake-to-camel.js
@@ -1,16 +1,16 @@
-function snakeToCamel(snakeKey) {
+function convertSnakeToCamel(snakeKey) {
   return snakeKey.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
 }
 
 function replaceSnakeToCamel(data) {
-  if (Array.isArray(data)) {
-    return data.map((element) => replaceSnakeToCamel(element));
-  } else if (data !== null && data.constructor === Object) {
-    return Object.fromEntries(
-      Object.entries(data).map(([key, value]) => [snakeToCamel(key), value])
-    );
-  }
-  return data;
+  return data.map((element) =>
+    Object.fromEntries(
+      Object.entries(element).map(([key, value]) => [
+        convertSnakeToCamel(key),
+        value,
+      ])
+    )
+  );
 }
 
 export { replaceSnakeToCamel };
