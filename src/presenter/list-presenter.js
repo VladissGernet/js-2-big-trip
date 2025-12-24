9c267063a857db8deda93e85291994ec32194914
VladissGernet <skeimorr@gmail.com>
2025-12-24 20:23:21 +0400

    Задокументировал и отрефакторил презентр

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -4,48 +4,86 @@ import ListCreationFormView from '../view/list-view/list-creation-form-view.js';
 
 import { render, RenderPosition } from '../framework/render.js';
 
+/**
+ * Создание динамического списка путевых точек
+ * @param {Array<Object>} listPoints Массив из объектов с данными путевых точек
+ * @param {Object<id, Object>} destinationsById Объект с данными о назначениях по ID
+ * @param {Object<id, Object>} offersByType Объект с данными о предложениях по типу
+ * @param {HTMLUlElement} element Контейнер для списка путевых точек
+ */
+const createList = ({
+  listPoints,
+  destinationsById,
+  offersByType,
+  element,
+}) => {
+  for (let i = 0; i < listPoints.length; i++) {
+    const pointData = listPoints[i];
+
+    const destinationData = destinationsById[pointData.destination];
+
+    const offerTypeData = offersByType[pointData.type];
+
+    const filteredOfferData = pointData.offers.reduce((acc, offer) => {
+      acc.push(offerTypeData[offer]);
+      return acc;
+    }, []);
+
+    const newWayPoint = new ListWaypointView({
+      listPoint: pointData,
+      destinationData: destinationData,
+      offerData: filteredOfferData,
+    });
+    render(newWayPoint, element);
+  }
+};
+
 export default class ListPresenter {
-  list = new ListView();
+  /**
+   * @type {ListView}
+   * Контейнер для списка путевых точек
+   */
+  #listView = new ListView();
+
+  /** @type {HTMLElement} Контейнер */
+  #container = null;
+
+  /**
+   * @typedef {Object} TripModel
+   * @property {Array} listPoints - Список путевых точек
+   * @property {Object} destinationsById - Назначения по ID
+   * @property {Object} offersByType - Предложения по типу
+   */
+
+  /** @type {TripModel} Модель поездки */
+  #tripModel = null;
 
   constructor({ container, tripModel }) {
-    this.container = container;
-    this.tripModel = tripModel;
+    this.#container = container;
+    this.#tripModel = tripModel;
   }
 
+  /**
+   * Инициализация презентера
+   */
   init() {
-    this.listPoints = this.tripModel.listPoints;
-    this.listDestinations = this.tripModel.destinationsById;
-    this.listOffers = this.tripModel.offersByType;
-
-    render(this.list, this.container);
+    const { listPoints, destinationsById, offersByType } = this.#tripModel;
+    render(this.#listView, this.#container);
 
     // Создание динамического списка.
-    for (let i = 0; i < this.listPoints.length; i++) {
-      const pointData = this.listPoints[i];
-
-      const destinationData = this.listDestinations[pointData.destination];
-
-      const offerTypeData = this.listOffers[pointData.type];
-
-      const filteredOfferData = pointData.offers.reduce((acc, offer) => {
-        acc.push(offerTypeData[offer]);
-        return acc;
-      }, []);
-
-      const newWayPoint = new ListWaypointView({
-        listPoint: pointData,
-        destinationData: destinationData,
-        offerData: filteredOfferData,
-      });
-      render(newWayPoint, this.list.element);
-    }
+    createList({
+      listPoints: listPoints,
+      destinationsById: destinationsById,
+      offersByType: offersByType,
+      element: this.#listView.element,
+    });
 
     // Добавление формы создания путевой точки.
     // На первое время добавляю просто первую точку из исписка.
-    const firstWayPointForBegin = this.listPoints[0];
+    const firstWayPointForBegin = listPoints[0];
     const firstDestinationData =
-      this.listDestinations[firstWayPointForBegin.destination];
-    const firstOffersTypeData = this.listOffers[firstWayPointForBegin.type];
+      destinationsById[firstWayPointForBegin.destination];
+    const firstOffersTypeData = offersByType[firstWayPointForBegin.type];
 
     render(
       new ListCreationFormView({
@@ -53,7 +91,7 @@ export default class ListPresenter {
         destinationData: firstDestinationData,
         listOffers: firstOffersTypeData,
       }),
-      this.list.element,
+      this.#listView.element,
       RenderPosition.AFTERBEGIN
     );
   }
