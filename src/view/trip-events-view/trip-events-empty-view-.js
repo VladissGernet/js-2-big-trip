d37eba31dcd18102390b46fdb54bd6f4fc094016
VladissGernet <skeimorr@gmail.com>
2026-04-11 14:41:16 +0400

    Подготовил коммент для решения бага

--- src/view/trip-events-view/trip-events-empty-view-.js
+++ src/view/trip-events-view/trip-events-empty-view-.js
@@ -14,6 +14,11 @@ export default class TripEventsEmptyView extends AbstractView {
   constructor(value) {
     super();
     this.#value = value;
+
+    // TODO, генерирует где-то два сообщения при пустом списке
+    // 1. нажатие кнопки создания New Event (зачем-то тут создание)
+    // 2. переключение фильтра
+    // далее уже отрисовка 2-го лишнего сообщения.
   }
 
   get template() {
