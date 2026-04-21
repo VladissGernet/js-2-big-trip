ec4967a997e21dc4394c42eaa9e2394961748f32
VladissGernet <skeimorr@gmail.com>
2026-04-21 21:45:29 +0400

    Закончил задание.

--- src/model/trip-model.js
+++ src/model/trip-model.js
@@ -111,7 +111,6 @@ export default class TripModel extends Observable {
   findDestinationByIndex(index) {
     // Дефолтная сортировка по датам.
     const sortedList = this.listPoints.toSorted(SORT_CONFIG['date']);
-
     return this.destinationsReadOnly?.find(
       ({ id }) => id === sortedList[index]?.destination,
     );
